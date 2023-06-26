937210b7f4beaa9462d54475be377568debd8c3d
ng-ha <ngthanhha.hn@gmail.com>
2023-06-26 23:57:43 +0700

    update

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,28 +1,37 @@
-'use client'
+"use client";
 
-import { useBoardStore } from '@/store/BoardStore'
-import React, { useEffect } from 'react'
-import {DragDropContext, Droppable} from 'react-beautiful-dnd'
+import { useBoardStore } from "@/store/BoardStore";
+import React, { useEffect } from "react";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 
 export default function Board() {
-  const getBoard = useBoardStore(state => state.getBoard)
-
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
   useEffect(() => {
-    getBoard()
-  }, [getBoard])
+    getBoard();
+  }, [getBoard]);
+
+  const handleOnDragEnd = (result: DropResult) => {};
 
   return (
-    'hi'
-    // <DragDropContext >
-    //     <Droppable droppableId='board' direction='horizontal' type='column'>
-    //         {(provided) => (
-    //             <div>
-                    
-    //             </div>
-    //         )}
-    //     </Droppable>
-      
-    // </DragDropContext>
-
-  )
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
--- src/store/BoardStore.ts
+++ src/store/BoardStore.ts
@@ -3,16 +3,16 @@ import { create } from "zustand";
 import { Board, Column, TypedColumns } from "../../typings";
 
 interface BoardState {
-    board: Board;
-    getBoard: () => void;
+  board: Board;
+  getBoard: () => void;
 }
 
 export const useBoardStore = create<BoardState>()((set) => ({
-    board: {
-        columns: new Map<TypedColumns, Column>()
-    },
-    getBoard: async () => {
-        const board = await getTodosGroupedByColumn()
-        set({board})
-    },
+  board: {
+    columns: new Map<TypedColumns, Column>(),
+  },
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
 }));
